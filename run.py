102eec1f7bf262975fd6bf19385e659d17bbb1f9
Tristan Fischer <sphere@dersphere.de>
2023-10-28 10:00:18 +0200

    refactor to use pathlib

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import shutil
-import os
 import sys
 import datetime
 from pathlib import Path
@@ -19,70 +17,66 @@ POTENTIAL_SOURCES = [
 
 TARGET_PARENT = '/Volumes/PortableSSD/Astrophotography'
 
-def _time_str(file):
-    return datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d')
+def _time_str(path: Path) -> str:
+    return datetime.datetime.fromtimestamp(path.stat().st_ctime).strftime('%Y-%m-%d')
 
-def get_source_folder():
-    folders = []
-    for source in POTENTIAL_SOURCES:
-        if os.path.exists(source):
-            for folder in os.listdir(source):
-                if folder not in EXCLUDED_FOLDERS:
-                    folders.append({
-                        'name': folder,
-                        'path': os.path.join(source, folder),
-                        'date': _time_str(os.path.join(source, folder)),
-                    })
-    if not folders:
+def get_source_path() -> Path:
+    paths = []
+    for source_path in map(Path, POTENTIAL_SOURCES):
+        if source_path.is_dir():
+            for path in source_path.iterdir():
+                if path.name not in EXCLUDED_FOLDERS:
+                    paths.append(path)
+    if not paths:
         print("No folders found!")
         sys.exit(1)
-    folders.sort(key=lambda x: x['date'], reverse=True)
-    folders_menu = [f'{folder["date"]} - {folder["path"]}' for folder in folders]
-    idx = TerminalMenu(folders_menu).show()
-    selected_folder = folders[idx]
-    return selected_folder
+    paths.sort(key=lambda x: x.stat().st_ctime, reverse=True)
+    idx = TerminalMenu([f'{_time_str(path)} - {path}' for path in paths]).show()
+    selected_path = paths[idx]
+    return selected_path
 
-def get_target_folder(source_folder):
-    if not os.path.exists(TARGET_PARENT):
+
+
+def get_target_path(source_path: Path) -> Path:
+    parent_path = Path(TARGET_PARENT)
+    if not parent_path.is_dir():
         print(f"Target folder {TARGET_PARENT} does not exist!")
         sys.exit(1)
 
     folders = []
 
     folders.append({
-        'name': 'Create new folder',
+        'name': 'Input folder name',
         'path': None,
     })
 
-    potential_target_folder_name = f'{source_folder["date"]} - {source_folder["name"]}'
-    new_str = ' (NEW)' if not os.path.exists(os.path.join(TARGET_PARENT, potential_target_folder_name)) else ''
-    folders.append({
-        'name': f'{potential_target_folder_name}{new_str}' ,
-        'path': os.path.join(TARGET_PARENT, potential_target_folder_name),
-    })
+    suggested_path = parent_path.joinpath(f'{_time_str(source_path)} - {source_path.name}')
+    if not suggested_path.is_dir():
+        folders.append({
+            'name': f'(NEW) {suggested_path.name}' ,
+            'path': suggested_path,
+        })
 
     folders.extend([{
-        'name': folder.name,
-        'path': os.path.join(TARGET_PARENT, folder),
-    } for folder in sorted(Path(TARGET_PARENT).iterdir(), key=os.path.getctime, reverse=True)])
+        'name': path.name,
+        'path': path,
+    } for path in sorted(parent_path.iterdir(), key=lambda x: x.stat().st_ctime, reverse=True) if not path.name in EXCLUDED_FOLDERS])
 
-    folders_menu = [folder["name"] for folder in folders]
-    idx = TerminalMenu(folders_menu).show()
+    idx = TerminalMenu([folder["name"] for folder in folders]).show()
     selected_folder = folders[idx]
     if selected_folder['path'] is None:
         new_folder_name = input("Enter new folder name: ")
-        selected_folder = {
-            'name': new_folder_name,
-            'path': os.path.join(TARGET_PARENT, new_folder_name),
-        }
-        os.mkdir(selected_folder['path'])
-    return selected_folder
+        target_path = parent_path.joinpath(new_folder_name)
+    else:
+        target_path = selected_folder['path']
+    if not target_path.is_dir():
+        target_path.mkdir()
+    return target_path
 
 def main():
-    #source_folder = get_source_folder()
-    source_folder = {'name': 'NGC188', 'path': '/Volumes/TF Images/ASIAIR/Autorun/Light/NGC188', 'date': '2023-10-27'}
+    source_folder = get_source_path()
     print(f"Using source {source_folder}!")
-    target_folder = get_target_folder(source_folder)
+    target_folder = get_target_path(source_folder)
     print(f"Using target {target_folder}!")
 
 if __name__ == "__main__":
