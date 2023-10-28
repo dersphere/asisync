88c2d4b9a45ae15d3ccb24878803c30122e1f5ef
Tristan Fischer <sphere@dersphere.de>
2023-10-28 11:05:48 +0200

    add copying and use black

--- run.py
+++ run.py
@@ -3,22 +3,27 @@
 import sys
 import datetime
 from pathlib import Path
+import re
+from shutil import copy, copy2, copystat
 
 from simple_term_menu import TerminalMenu
 
 EXCLUDED_FOLDERS = [
-    '.DS_Store',
+    ".DS_Store",
 ]
 
 POTENTIAL_SOURCES = [
-    '/Volumes/TF Images/ASIAIR/Autorun/Light',
-    '/Volumes/TF Images/ASIAIR/Plan/Light',
+    "/Volumes/TF Images/ASIAIR/Autorun/Light",
+    "/Volumes/TF Images/ASIAIR/Plan/Light",
 ]
 
-TARGET_PARENT = '/Volumes/PortableSSD/Astrophotography'
+TARGET_PARENT = "/Volumes/PortableSSD/Astrophotography"
+INCLUDE_PATTERN = re.compile(r"^(.+)\.((fit)|(fits))$")
+
 
 def _time_str(path: Path) -> str:
-    return datetime.datetime.fromtimestamp(path.stat().st_ctime).strftime('%Y-%m-%d')
+    return datetime.datetime.fromtimestamp(path.stat().st_ctime).strftime("%Y-%m-%d")
+
 
 def get_source_path() -> Path:
     paths = []
@@ -31,10 +36,9 @@ def get_source_path() -> Path:
         print("No folders found!")
         sys.exit(1)
     paths.sort(key=lambda x: x.stat().st_ctime, reverse=True)
-    idx = TerminalMenu([f'{_time_str(path)} - {path}' for path in paths]).show()
+    idx = TerminalMenu([f"{_time_str(path)} - {path}" for path in paths]).show()
     selected_path = paths[idx]
     return selected_path
-
 
 
 def get_target_path(source_path: Path) -> Path:
@@ -45,39 +49,88 @@ def get_target_path(source_path: Path) -> Path:
 
     folders = []
 
-    folders.append({
-        'name': 'Input folder name',
-        'path': None,
-    })
+    folders.append(
+        {
+            "name": "Input folder name",
+            "path": None,
+        }
+    )
 
-    suggested_path = parent_path.joinpath(f'{_time_str(source_path)} - {source_path.name}')
+    suggested_path = parent_path.joinpath(
+        f"{_time_str(source_path)} - {source_path.name}"
+    )
     if not suggested_path.is_dir():
-        folders.append({
-            'name': f'(NEW) {suggested_path.name}' ,
-            'path': suggested_path,
-        })
+        folders.append(
+            {
+                "name": f"(NEW) {suggested_path.name}",
+                "path": suggested_path,
+            }
+        )
 
-    folders.extend([{
-        'name': path.name,
-        'path': path,
-    } for path in sorted(parent_path.iterdir(), key=lambda x: x.stat().st_ctime, reverse=True) if not path.name in EXCLUDED_FOLDERS])
+    folders.extend(
+        [
+            {
+                "name": path.name,
+                "path": path,
+            }
+            for path in sorted(
+                parent_path.iterdir(), key=lambda x: x.stat().st_ctime, reverse=True
+            )
+            if not path.name in EXCLUDED_FOLDERS
+        ]
+    )
 
     idx = TerminalMenu([folder["name"] for folder in folders]).show()
     selected_folder = folders[idx]
-    if selected_folder['path'] is None:
+    if selected_folder["path"] is None:
         new_folder_name = input("Enter new folder name: ")
         target_path = parent_path.joinpath(new_folder_name)
     else:
-        target_path = selected_folder['path']
+        target_path = selected_folder["path"]
     if not target_path.is_dir():
         target_path.mkdir()
     return target_path
+
+
+def get_source_files(source_path: Path) -> list[Path]:
+    return sorted(
+        [
+            path
+            for path in source_path.iterdir()
+            if path.is_file() and INCLUDE_PATTERN.match(path.name)
+        ],
+        key=lambda x: x.stat().st_ctime,
+    )
+
+
+def copy_file_if_not_exists(source_file: Path, target_file: Path) -> bool:
+    if (
+        target_file.is_file()
+        and source_file.stat().st_size == target_file.stat().st_size
+    ):
+        # print(f"Skipping {source_file.name}")
+        return False
+    print(f"Copying {source_file.name} ...")
+    copy(source_file, target_file)
+    try:
+        copystat(source_file, target_file)
+    except PermissionError:
+        # some or all permissions could not be copied. Sometimes times work and just permissions fail
+        pass
+    return True
+
 
 def main():
     source_folder = get_source_path()
     print(f"Using source {source_folder}!")
     target_folder = get_target_path(source_folder)
     print(f"Using target {target_folder}!")
+    while True:
+        source_files = get_source_files(source_folder)
+        for source_file in source_files:
+            target_file = target_folder.joinpath(source_file.name)
+            copy_file_if_not_exists(source_file, target_file)
+
 
 if __name__ == "__main__":
     main()
